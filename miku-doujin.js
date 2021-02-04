7cf7849daaf71db5290d6e82a8107a2003edbfeb
Varkaria <61514399+Varkaria@users.noreply.github.com>
2021-02-04 23:28:45 +0700

    Create miku-doujin.js

---
+++ miku-doujin.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Miku-doujin Ads Remover
+// @namespace    https://miku-doujin.com/*
+// @version      0.1
+// @description  try to take over the world!
+// @author       Varkaria sukinami
+// @match        https://miku-doujin.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    document.getElementsByClassName('text-center mb-3')[0].remove()
+    document.getElementsByClassName('text-center mb-3')[0].remove() //wtf are you serious!!
+})();
